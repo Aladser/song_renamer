49877c340cf23c1155b29270083c40dfe2027f69
Aladser <aladser@gmail.com>
2022-04-16 15:33:46 +0900

    1002 поддержка flac

---
+++ Mp3RenamerV2/MainFrame.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Mp3RenamerV2
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrame));
+            this.infoField = new System.Windows.Forms.RichTextBox();
+            this.openFileMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkTagsMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkTagsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkNameMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkFileNameMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkAlbFileNameMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьПолеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkLabel = new System.Windows.Forms.Label();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // infoField
+            // 
+            this.infoField.Location = new System.Drawing.Point(0, 57);
+            this.infoField.Name = "infoField";
+            this.infoField.Size = new System.Drawing.Size(1096, 477);
+            this.infoField.TabIndex = 3;
+            this.infoField.Text = "";
+            // 
+            // openFileMS
+            // 
+            this.openFileMS.Name = "openFileMS";
+            this.openFileMS.Size = new System.Drawing.Size(108, 22);
+            this.openFileMS.Text = "Файл";
+            this.openFileMS.Click += new System.EventHandler(this.openFileMenuItem_Click);
+            // 
+            // openFolderMS
+            // 
+            this.openFolderMS.Name = "openFolderMS";
+            this.openFolderMS.Size = new System.Drawing.Size(108, 22);
+            this.openFolderMS.Text = "Папку";
+            this.openFolderMS.Click += new System.EventHandler(this.openFolderMenuItem_Click);
+            // 
+            // checkTagsMS
+            // 
+            this.checkTagsMS.Name = "checkTagsMS";
+            this.checkTagsMS.Size = new System.Drawing.Size(105, 20);
+            this.checkTagsMS.Text = "Проверить теги";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenuItem,
+            this.checkTagsMenuItem,
+            this.checkNameMenuItem,
+            this.очиститьПолеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1108, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openMenuItem
+            // 
+            this.openMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileMenuItem,
+            this.openFolderMenuItem});
+            this.openMenuItem.Name = "openMenuItem";
+            this.openMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.openMenuItem.Text = "Открыть";
+            // 
+            // openFileMenuItem
+            // 
+            this.openFileMenuItem.Name = "openFileMenuItem";
+            this.openFileMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFileMenuItem.Text = "файл";
+            this.openFileMenuItem.Click += new System.EventHandler(this.openFileMenuItem_Click);
+            // 
+            // openFolderMenuItem
+            // 
+            this.openFolderMenuItem.Name = "openFolderMenuItem";
+            this.openFolderMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFolderMenuItem.Text = "папку";
+            this.openFolderMenuItem.Click += new System.EventHandler(this.openFolderMenuItem_Click);
+            // 
+            // checkTagsMenuItem
+            // 
+            this.checkTagsMenuItem.Enabled = false;
+            this.checkTagsMenuItem.Name = "checkTagsMenuItem";
+            this.checkTagsMenuItem.Size = new System.Drawing.Size(105, 20);
+            this.checkTagsMenuItem.Text = "Проверить теги";
+            this.checkTagsMenuItem.Click += new System.EventHandler(this.checkTagsMenuItem_Click);
+            // 
+            // checkNameMenuItem
+            // 
+            this.checkNameMenuItem.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.checkNameMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkFileNameMenuItem,
+            this.checkAlbFileNameMenuItem});
+            this.checkNameMenuItem.Enabled = false;
+            this.checkNameMenuItem.Name = "checkNameMenuItem";
+            this.checkNameMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.checkNameMenuItem.Text = "Проверить имя";
+            // 
+            // checkFileNameMenuItem
+            // 
+            this.checkFileNameMenuItem.Name = "checkFileNameMenuItem";
+            this.checkFileNameMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.checkFileNameMenuItem.Text = "файла";
+            this.checkFileNameMenuItem.Click += new System.EventHandler(this.checkFileNameMenuItem_Click);
+            // 
+            // checkAlbFileNameMenuItem
+            // 
+            this.checkAlbFileNameMenuItem.Name = "checkAlbFileNameMenuItem";
+            this.checkAlbFileNameMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.checkAlbFileNameMenuItem.Text = "файла альбома";
+            this.checkAlbFileNameMenuItem.Click += new System.EventHandler(this.checkAlbFileNameMenuItem_Click);
+            // 
+            // очиститьПолеToolStripMenuItem
+            // 
+            this.очиститьПолеToolStripMenuItem.Name = "очиститьПолеToolStripMenuItem";
+            this.очиститьПолеToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.очиститьПолеToolStripMenuItem.Text = "Очистить поле";
+            this.очиститьПолеToolStripMenuItem.Click += new System.EventHandler(this.clearInfoField_Click);
+            // 
+            // checkLabel
+            // 
+            this.checkLabel.Location = new System.Drawing.Point(12, 24);
+            this.checkLabel.Name = "checkLabel";
+            this.checkLabel.Size = new System.Drawing.Size(100, 23);
+            this.checkLabel.TabIndex = 5;
+            this.checkLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.Location = new System.Drawing.Point(996, 24);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(100, 23);
+            this.progressLabel.TabIndex = 6;
+            this.progressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1108, 546);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.checkLabel);
+            this.Controls.Add(this.infoField);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFrame";
+            this.Text = "SongRenamer 2.01";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private RichTextBox infoField;
+        private ToolStripMenuItem openFileMS;
+        private ToolStripMenuItem openFolderMS;
+        private ToolStripMenuItem checkFileNameMS;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem openMenuItem;
+        private ToolStripMenuItem openFileMenuItem;
+        private ToolStripMenuItem openFolderMenuItem;
+        private ToolStripMenuItem checkTagsMenuItem;
+        private ToolStripMenuItem checkTagsMS;
+        private ToolStripMenuItem checkNameMenuItem;
+        private ToolStripMenuItem checkFileNameMenuItem;
+        private ToolStripMenuItem checkAlbFileNameMenuItem;
+        private Label checkLabel;
+        private ToolStripMenuItem очиститьПолеToolStripMenuItem;
+        private Label progressLabel;
+    }
+}
---
+++ Mp3RenamerV2/MainFrame.cs
@@ -0,0 +1,359 @@
+using System.ComponentModel;
+using System.Linq;
+using System.Security;
+using System.Text;
+using TagLib;
+
+namespace Mp3RenamerV2
+{
+    public partial class MainFrame : Form
+    {
+        /// <summary>
+        /// Путь открытому файлу или папке
+        /// </summary>
+        private String selectedPath;
+        /// <summary>
+        /// Флаг открытого файла или папки
+        /// </summary>
+        private bool isSelectedFile = true; // Флаг выбора файла или папки
+        private bool isFirstString = false; // Флаг напечатанной первой строки
+        private OpenFileDialog openFileDialog;
+        private FolderBrowserDialog openFolderDialog;
+        private String startPath = "";
+        private BackgroundWorker bw; // фоновый поток
+
+        public MainFrame()
+        {
+            InitializeComponent();
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "mp3-файлы (*.mp3)|*.mp3|flac-файлы (*.flac)|*.flac|Все файлы (*.*)|*.*";
+            openFolderDialog = new FolderBrowserDialog();
+            updateStartPath(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+            checkTagsMS.Enabled = false;
+        }
+
+        // Событие Открыть файл
+        private void openFileMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                checkLabel.Text = "";
+                checkTagsMenuItem.Enabled = true;
+                checkNameMenuItem.Enabled = true;
+                if (!isFirstString) isFirstString = true;
+                else print("\n");
+
+                isSelectedFile = true;
+                selectedPath = openFileDialog.FileName;
+                print(selectedPath + "\n");
+                print("----- " + showTags(selectedPath));
+                updateStartPath(selectedPath.Substring(0, selectedPath.Length - openFileDialog.SafeFileName.Length));   
+            }
+        }
+        // Событие Открыть папку
+        private void openFolderMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFolderDialog.ShowDialog() != DialogResult.OK) return;
+            checkLabel.Text = "Выполнение";
+            checkTagsMenuItem.Enabled = true;
+            checkNameMenuItem.Enabled = true;
+            if (!isFirstString) isFirstString = true;
+            else print("\n");
+
+            isSelectedFile = false;
+            selectedPath = openFolderDialog.SelectedPath;
+            print("Папка: " + selectedPath + "\n");
+
+            bw = new BackgroundWorker();
+            bw.WorkerSupportsCancellation = true;
+            bw.WorkerReportsProgress = true;
+            bw.ProgressChanged += progressChangedBW;
+            bw.RunWorkerCompleted += runCompletedBW;
+            bw.DoWork += openFolder;
+            bw.RunWorkerAsync();
+        }
+        // Открыть папку
+        private void openFolder(object sender, DoWorkEventArgs e)
+        {
+            String[] folderFileElements = Directory.GetFileSystemEntries(selectedPath);// элементы папки
+            int progLength = 2*folderFileElements.Length;
+            int prog = 0;
+            // показ папок
+            foreach (String elem in folderFileElements)
+            {
+                if (Path.GetExtension(elem) == "") print(elem+"\n");
+                bw.ReportProgress(++prog * 100/ progLength);
+            }
+            // показ мp3 файлов
+            string ext;
+            foreach (String elem in folderFileElements)
+            {
+                ext = Path.GetExtension(elem);
+                if (ext==".mp3" || ext==".flac")
+                {
+                    print(elem + "\n");
+                    print("----- " + showTags(elem));
+                }
+                bw.ReportProgress(++prog * 100 / progLength);
+            }
+            // Путь корневой папки
+            String[] foldersInPath = selectedPath.Split("\\"); // число папок в пути
+            int rootFolderPathLength = 0;
+            for (int i = 0; i < foldersInPath.Length - 1; i++)
+                rootFolderPathLength += foldersInPath[i].Length + 1; // с учетом \
+            updateStartPath(selectedPath.Substring(0, rootFolderPathLength));
+            e.Cancel = true;
+        }
+        // Событие проверки тегов
+        private void checkTagsMenuItem_Click(object sender, EventArgs e)
+        {
+            // файла
+            if (isSelectedFile)
+            {
+                checkLabel.Text = "";
+                selectedPath = deleteRedudantSymbols(selectedPath);
+                checkTags(selectedPath);
+            }
+            // папка
+            else
+            {
+                checkLabel.Text = "Выполнение";
+                bw = new BackgroundWorker();
+                bw.WorkerSupportsCancellation = true;
+                bw.WorkerReportsProgress = true;
+                bw.ProgressChanged += progressChangedBW;
+                bw.RunWorkerCompleted += runCompletedBW;
+                bw.DoWork += checkTagsBW;
+                bw.RunWorkerAsync();
+                bw.CancelAsync();
+            }
+        }
+        // Фоновая задача проверки тегов
+        private void checkTagsBW(object sender, DoWorkEventArgs e)
+        {
+            String[] folderFileElements = Directory.GetFileSystemEntries(selectedPath);
+            for (int i = 0; i < folderFileElements.Length; i++)
+            {
+                folderFileElements[i] = deleteRedudantSymbols(folderFileElements[i]);
+                checkTags(folderFileElements[i]);
+                bw.ReportProgress((i+1)*100/folderFileElements.Length);
+            }
+            e.Cancel = true;
+        }
+        // Проверяет наличие тегов Артист-Название и заполняет их из имени файла
+        private bool checkTags(String file)
+        {
+            string ext = Path.GetExtension(file);
+            if (ext != ".mp3" && ext != ".flac") return false;
+            String str1 = showTags(file);
+            TagLib.File tags = TagLib.File.Create(file);
+            // Проверяет название песни
+            if (tags.Tag.Title == null)
+            {
+                int arg1 = file.IndexOf("-") + 2; // начало выреза
+                int arg2 = file.Length - arg1 - 4; // число символов для выреза
+                tags.Tag.Title = file.Substring(arg1, arg2); ;
+                tags.Save();
+            }
+            // Проверяет исполнителя
+            if (tags.Tag.FirstPerformer == null)
+            {
+                String fileName = Path.GetFileName(file);
+                tags.Tag.Performers = new String[1] { fileName.Substring(0, fileName.IndexOf("-") - 1) };
+                tags.Save();
+            }
+            String str2 = showTags(file);
+            if (!str1.Equals(str2)) print(str2);
+            else print("Правильные теги\n");
+            return true;
+        }
+        // Событие Проверка имени файла
+        private void checkFileNameMenuItem_Click(object sender, EventArgs e)
+        {
+            selectedPath = checkFileName_Click(selectedPath, false);
+        }
+        // Событие Проверка имени файла альбома
+        private void checkAlbFileNameMenuItem_Click(object sender, EventArgs e)
+        {
+            selectedPath = checkFileName_Click(selectedPath, true);
+        }
+        // Проверяет и изменяет имя файла на соответствие тегам
+        private string checkFileName_Click(string filename, bool isAlbum)
+        {
+            if (isSelectedFile)
+            {
+                string newname = checkFileName(filename, isAlbum);
+                if (newname == null) return filename;
+                if (!filename.Equals(newname))
+                {
+                    filename = newname;
+                    print(filename + "\n");
+                }
+                else
+                    print("Название соотвествует тегам\n");
+            }
+            else
+            {
+                checkLabel.Text = "Выполнение";
+
+                bw = new BackgroundWorker();
+                bw.DoWork += checkFileNameBW;
+
+                bw.WorkerSupportsCancellation = true;
+                bw.WorkerReportsProgress = true;
+                bw.ProgressChanged += progressChangedBW;
+                bw.RunWorkerCompleted += runCompletedBW;
+
+                bw.RunWorkerAsync(isAlbum);
+                bw.CancelAsync();
+            }
+            return filename;
+        }
+        // Фоновая задача проверки имени файла
+        private void checkFileNameBW(object sender, DoWorkEventArgs e)
+        {
+            String[] folderFileElements = Directory.GetFileSystemEntries(selectedPath);
+            string newname, ext;
+            for (int i = 0; i < folderFileElements.Length; i++)
+            {
+                ext = Path.GetExtension(folderFileElements[i]);
+                if (ext!=".mp3" && ext!=".flac") continue;
+                newname = checkFileName(folderFileElements[i], (bool)e.Argument);
+                if (!folderFileElements[i].Equals(newname))
+                {
+                    folderFileElements[i] = newname;
+                    print(folderFileElements[i]+"\n");
+                }
+                else
+                    print("Название соотвествует тегам\n");
+                bw.ReportProgress((i+1) * 100 / folderFileElements.Length);
+            }
+            e.Cancel = true;
+        }
+        /// <summary>
+        /// Проверяет и изменяет имя файла на соответствие тегам
+        /// </summary>
+        private string checkFileName(String filename, bool isAlbum)
+        {
+            string newname = filename.Substring(0, filename.Length - Path.GetFileName(filename).Length);
+            TagLib.File tags = TagLib.File.Create(filename);
+            // Проверяется наличие тегов
+            if(tags.Tag.Performers == null)
+            {
+                print("Есть пустые теги");
+                return null;
+            }
+            if (tags.Tag.Title == null)
+            {
+                print("Есть пустые теги");
+                return null;
+            }
+            // Записывается номер трека для файла из альбома
+            if (isAlbum)
+            {
+                if (tags.Tag.Track < 10) newname += "0" + tags.Tag.Track + ". ";
+                else newname += tags.Tag.Track + ". ";
+            }
+            // Создается имя файла из тегов
+            newname += tags.Tag.Performers[0] + " - " + tags.Tag.Title;
+            newname += Path.GetExtension(filename) == ".mp3" ? ".mp3" : ".flac";
+            {
+                 try
+                 {
+                    System.IO.File.Move(filename, newname);
+                 }
+                 catch (System.IO.DirectoryNotFoundException exc)
+                 {
+                    print(exc.Message);
+                 }
+                  filename = newname;
+            }
+            newname = deleteRedudantSymbols(filename);
+            if (!filename.Equals(newname))
+            {
+                System.IO.File.Move(filename, newname);
+                filename = newname;
+            }
+            return filename;
+        }
+        /// <summary>
+        /// Отображает прогресс фоновой задачи
+        /// </summary>
+        private void progressChangedBW(object sender, ProgressChangedEventArgs e)
+        {
+           progressLabel.Text = (e.ProgressPercentage.ToString() + "%");
+        }
+        /// <summary>
+        /// Событие завершения фоновой задачи
+        /// </summary>
+        private void runCompletedBW(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                checkLabel.Text = "Готово";
+            }
+        }
+        /// <summary>
+        /// Показывает теги файла
+        /// </summary>
+        private String showTags(String file)
+        {
+            String res = "";
+            TagLib.File tagSong = TagLib.File.Create(file);
+            res += "{";
+            res += tagSong.Tag.FirstPerformer == null ? "Пусто" : tagSong.Tag.FirstPerformer;
+            res += "} - {";
+            res += tagSong.Tag.Title == null ? "Пусто" : tagSong.Tag.Title;
+            res += "}\n";
+            return res;
+        }
+        /// <summary>
+        /// Обновляет точку открытия openFileDialog
+        /// </summary>
+        private void updateStartPath(String value)
+        {
+            startPath = value;
+            openFileDialog.InitialDirectory = startPath;
+            openFolderDialog.InitialDirectory = startPath;
+        }
+        /// <summary>
+        /// Выводит информацию в infoField
+        /// </summary>
+        private void print(String text)
+        {
+            infoField.Invoke(new Action(() => { infoField.Text += text; }));
+        }
+        /// <summary>
+        /// Удаляет лишние символы из названий
+        /// </summary>
+        private string deleteRedudantSymbols(string filename)
+        {
+            String newName;
+            // Удаляет из имени файла -kissvk.com
+            String kissVK = "-kissvk.com";
+            if (filename.Contains(kissVK))
+            {
+                newName = filename.Remove(filename.IndexOf(kissVK), kissVK.Length);
+                System.IO.File.Move(filename, newName);
+                filename = newName;
+            }
+            // редактирует " - "
+            if (filename.Contains("-") && !filename.Contains(" - "))
+            {
+                {
+                    newName = filename.Replace("-", " - ");
+                    System.IO.File.Move(filename, newName);
+                    filename = newName;
+                }
+            }
+            return filename;
+        }
+        /// <summary>
+        /// Очищает infoField
+        /// </summary>
+        private void clearInfoField_Click(object sender, EventArgs e)
+        {
+            infoField.Text = "";
+        }
+    }
+}
